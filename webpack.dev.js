829cec9215cba0f6e8c91ca67040a75d777f0f95
Daniele Campari <danielecampari@gmail.com>
2022-10-14 12:16:55 +0200

    fix wp-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const webpack = require('webpack');
-
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
@@ -21,9 +19,5 @@ module.exports = merge(common, {
                 use: ['style-loader', 'css-loader']
             }
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
-
+    }
 });
