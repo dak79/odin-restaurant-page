58cb74015bc3465667c93980f099ee3cb5c43499
Daniele Campari <danielecampari@gmail.com>
2022-10-14 10:33:55 +0200

    add template.html, wp-server and wp-clean-plug

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        clean: true
-    },
-    devtool: mode === 'development' ? 'inline-source-map' : false,
-    rules: [
-        {
-            test: /\.m?js$/i,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
+module.exports = (env, argv) => {
+    return {
+        mode: env.production ? 'production' : 'development',
+        entry: {
+            index: './src/index.js',
         },
-        {
-            test: /\.html$/i,
-            type: 'asset/resource'
-        }
-    ],
-    plugin: [
-        new HtmlWebpackPlugin({
-            title: 'Restaurant Page',
-            template: './src/index.html',
-            //favicon: path,
-            minify: true
-        })
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new HtmlMinimizerPlugin()
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].bundle.js',
+            clean: true
+        },
+        devtool: argv.mode === 'development' ? 'inline-source-map' : false,
+        devServer: {
+            static: './dist'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/i,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                          presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Restaurant Page',
+                template: './src/html/template.html',
+                filename: './index.html',
+                minify: true
+            }),
+            new CleanWebpackPlugin()
+
         ]
     }
 }
