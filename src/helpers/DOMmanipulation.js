c90d3347e10a34142fef845346cccc0d946d5d7c
Daniele Campari <danielecampari@gmail.com>
2022-10-19 10:32:14 +0200

    fix createList helper function

--- src/helpers/DOMmanipulation.js
+++ src/helpers/DOMmanipulation.js
@@ -1,25 +1,49 @@
 export const content = document.querySelector('#content');
 
+/**
+ * 
+ * @param { Node } element 
+ * @returns A new element
+ */
 export const create = element => document.createElement(element);
 
+/**
+ * 
+ * @param { Node } element - Element which needs multiple attributes.
+ * @param { Objects } attributes - Object of attributes, attr name: attr value. 
+ * @returns Elements with multiple attributes.
+ */
 const setAttributes = (element, attributes) => Object.keys(attributes).forEach(attr => element.setAttribute(attr, attributes[attr]));
 
-export const createList = (length, text) => {
+/**
+ * 
+ * @param { Node } parent - Parent Node
+ * @param { Array } children - Children Nodes
+ * @returns nodeList of children
+ */
+export const appendChildren = (parent, children) => children.forEach(child => parent.appendChild(child));
+
+/**
+ * 
+ * @param { Array } menuTitles - Titles nav bar items
+ * @returns Nav bar
+ */
+export const createList = (menuTitles) => {
     const list = create('ul');
     list.classList.add('menu');
-    for (let i = 0; i < length; i++) {
+
+    menuTitles.forEach(title => {
         const listItem = create('li');
         const listBtn = create('button');
         setAttributes(listBtn, {
             type: 'button',
-            id: `btn-${text[i].replace(' ', '-').toLowerCase().trim()}`,
+            id: `btn-${title.replace(' ', '-').toLowerCase().trim()}`,
             class: 'btn-menu'
-        });
-
-        listBtn.textContent = text[i];
+        })
+        listBtn.textContent = title;
         listItem.appendChild(listBtn);
         list.appendChild(listItem);
-    }
-
+    })
+    
     return list
 }
--- src/pages/header.js
+++ src/pages/header.js
@@ -1,14 +1,14 @@
-import { content, create, createList } from '../helpers/DOMmanipulation.js';
+import { content, create, createList, appendChildren } from '../helpers/DOMmanipulation.js';
 import '../styles/header.css';
 
 export default function header() {
     const header = create('header');
     const nav = create('nav');
-    const menu = createList(4, ['Home', 'About Us', 'Menu', 'Contact']);
+    const menu = createList(['Home', 'About Us', 'Menu', 'Contact']);
     const main = create('main');
-
-    content.appendChild(header);
+    main.setAttribute('id', '#display-page')
+    
+    appendChildren(content, [header, main]);
     header.appendChild(nav);
     nav.appendChild(menu);
-    content.appendChild(main);
 }
